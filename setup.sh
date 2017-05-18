87b416eef095adad6fa2a6d4ac3c28b710b826fc
John Nguyen <jolnguyen@ucdavis.edu>
2017-05-18 15:47:53 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,6 @@
 
 rm ~/.zshrc ~/.gitconfig ~/.vimrc ~/.tmux
 
-# install brew and zsh 
-ruby -e "$(curl -fsSL https://raw.zshhubusercontent.com/Homebrew/install/master/install)"
-brew doctor
-brew install zsh
-sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
-
 git clone https://github.com/JohnlNguyen/dotfiles.git ~/.dotfiles
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
