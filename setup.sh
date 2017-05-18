995634cfcb20ca860eb1d05fe319e42af52b1e4d
John Nguyen <jolnguyen@ucdavis.edu>
2017-05-18 16:05:37 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ git clone https://github.com/JohnlNguyen/dotfiles.git ~/.dotfiles
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/zfunc ~/.zfunc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
