3e110b97c7049f9d65c2b8505487bbb8a9c9c080
John Nguyen <jolnguyen@ucdavis.edu>
2017-05-18 15:46:27 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ brew doctor
 brew install zsh
 sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
 
-https://github.com/JohnlNguyen/dotfiles.git ~/.dotfiles
+git clone https://github.com/JohnlNguyen/dotfiles.git ~/.dotfiles
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
